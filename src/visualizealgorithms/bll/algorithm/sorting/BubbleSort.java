4d7602970058bf4958ec51fc277360c2ddafd2f4
scupak <anto1095@easv365.dk>
2020-03-10 14:29:21 +0100

    Update BubbleSort.java

--- src/visualizealgorithms/bll/algorithm/sorting/BubbleSort.java
+++ src/visualizealgorithms/bll/algorithm/sorting/BubbleSort.java
@@ -22,7 +22,35 @@ public class BubbleSort extends GenericAlgorithm {
     public void doWork() {
 
         int[] arr = (int[]) super.getData();
+        
+         for (int i = 1; i < arr.length; i++) {
+            
+             for (int j = 0; j < arr.length -i; j++) {
+                
+                 if(arr[j] > arr[j + 1])
+                 {
+                     int tmp = arr[j];
+                     arr[j] = arr[j +1];
+                     arr[j + 1] = tmp;
+                     
+                 
+                 
+                 }
+                 
+             }
+            
+        }
+         {
+         
+         
+         
+         }
+        
+        
+        
+        
+        }
 
-        //FIXME: implement algorithm...
+        
     }
-}
+
